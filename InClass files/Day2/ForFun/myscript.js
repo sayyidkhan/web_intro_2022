7d7079a186554a2a7af78172c1f957beafb412b4
MaryCao <caowanyue97@gmail.com>
2022-01-29 14:10:43 +0800

    Add In-Class Files

---
+++ InClass files/Day2/ForFun/myscript.js
@@ -0,0 +1,20 @@
+window.onload = function codeAddress() {
+
+    document.getElementById('redDot').onclick = function() { 
+        document.getElementById("dress").style.borderBottom="120px solid red";
+    };
+    document.getElementById('blueDot').onclick = function() { 
+        document.getElementById("dress").style.borderBottom="120px solid blue";
+    };
+    document.getElementById('greenDot').onclick = function() { 
+        document.getElementById("dress").style.borderBottom="120px solid green";
+    };
+    document.getElementById('blackDot').onclick = function() { 
+        document.getElementById("dress").style.borderBottom="120px solid black";
+    };
+    
+    
+    
+}
+
+
